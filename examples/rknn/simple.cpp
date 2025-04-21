b102f03b84f7f80261c653caf15c10880b53d130
xieli <xieli@microbt.com>
2025-04-21 11:00:39 +0800

    chore(matrix_mul): copy offical test-mul-mat0

--- examples/rknn/simple.cpp
+++ examples/rknn/simple.cpp
@@ -52,6 +52,8 @@ struct ggml_cgraph * build_graph(const simple_model& model) {
     struct ggml_cgraph  * gf = ggml_new_graph(model.ctx);
 
     // result = a*b^T
+    // Pay attention: ggml_mul_mat(A, B) ==> B will be transposed internally
+    // the result is transposed
     struct ggml_tensor * result = ggml_mul_mat(model.ctx, model.a, model.b);
 
     ggml_build_forward_expand(gf, result);
